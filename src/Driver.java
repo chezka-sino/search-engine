d9e620d01f709b12e92520fb9d2dec116f308f35
chezka-sino <msino@dons.usfca.edu>
2016-09-16 01:04:23 -0700

    first edit

--- src/Driver.java
+++ src/Driver.java
@@ -5,5 +5,7 @@ public class Driver {
     public static void main(String[] args) {
         // TODO Modify as necessary!
         System.out.println(Arrays.toString(args));
+        
+        IndexTest test = new IndexTest();
     }
 }
---
+++ src/IndexTest.java
@@ -0,0 +1,11 @@
+
+public class IndexTest {
+
+	/*
+	 * defines the implementation of IndexTest
+	 */
+	public IndexTest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
