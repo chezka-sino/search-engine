776bca31ffd4b6aca3c2f0ddea2f7df3cc82f186
chezka-sino <msino@dons.usfca.edu>
2016-09-17 08:21:50 -0700

    removed ArgumentParser.java, started with directory traversal

--- src/DirectoryTraverse.java
+++ src/DirectoryTraverse.java
@@ -1,5 +1,53 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class DirectoryTraverse {
 	
+	private final ArrayList<String> textFiles;
+//	private Map<String, String> argMap;
+	public Path dir;
+	
+	public DirectoryTraverse(String directory) {
+		// TODO Auto-generated constructor stub
+
+		dir = Paths.get(directory);
+		textFiles = new ArrayList<>();
+		Path path = Paths.get(directory);
+		
+	
+	}
+	
+	public void traverse(Path path) throws IOException {
+		
+		try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)) {
+			
+			for (Path file: listing) {
+				
+				if (Files.isDirectory(file)) {
+					traverse(file);
+				}
+				
+				else {
+					if (file.endsWith(".txt")) {
+						textFiles.add(file.toAbsolutePath().normalize().toString());
+					}
+				}
+			}
+			
+		}
+		
+	}
+	
+	public Path getDir() {
+		return dir;
+	}
+	
+	public ArrayList<String> getFileList () {
+		return textFiles;
+	}
 
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +11,7 @@ public class Driver {
 	
 	private static Map<String, String> argumentMap;
 	private static final String DEFAULT_INDEX = "index.json";
+	private static String directory;
 	
 	public static boolean isFlag(String arg) {
 		if (arg.startsWith("-")) {
@@ -44,15 +49,52 @@ public class Driver {
 		}
 	}
 	
-    public static void main(String[] args) {
+	// TODO what if no directory
+	
+	
+    public static void main(String[] args) throws IOException {
         // TODO Modify as necessary!
 //        System.out.println(Arrays.toString(args))
+    	
+    	
 
     	argumentMap = new HashMap<>();
+    	parseArguments(args);
+    	checkJSONPath();
     	
-    	ArgumentParser parse = new ArgumentParser(args);
-    	parse.parseArguments(args);
-    	parse.checkJSONPath();
+//    	if (!argumentMap.containsKey("-dir")) {
+//    		System.err.println("No directory provided");
+//    	}
+//    	
+//    	else {
+//    		DirectoryTraverse dir = new DirectoryTraverse(argumentMap.get("-dir"));
+//    	}
+    	
+    	try {
+//    		File file = new File(argumentMap.get("-dir"));
+    		
+    		DirectoryTraverse dir = new DirectoryTraverse(argumentMap.get("-dir"));
+    		dir.traverse(dir.getDir());
+    	}
+    	
+    	catch (NullPointerException | NoSuchFileException e) {
+    		System.err.println("Directory error");
+    	}
+    	
+//    	ArgumentParser parse = new ArgumentParser(args);
+//    	parse.parseArguments(args);
+//    	parse.checkJSONPath();
+//    	
+//    	if (!parse.hasDirectory()) {
+//    		System.err.println("No directory path was found");
+//    		System.exit(0);
+//    	}
+    	
+//    	DirectoryTraverse dir = new DirectoryTraverse(parse.getMap());
+//    	dir.traverse(dir.getDir());
+//    	
+//    	ArrayList<String> txt = dir.getFileList();
+//    	System.out.println(txt.toString());
 //    	parseArguments(args);
 //    	checkJSONPath();
     	
