1b18a7476630f3a01b286b1ab982b5002bd5f0c6
chezk <chezk@10.1.86.79>
2016-09-16 20:12:07 -0700

    edited args

--- src/Driver.java
+++ src/Driver.java
@@ -1,26 +1,49 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver { 
 	
+	private static Map<String, String> argMap;
+	
+	public static boolean isFlag(String arg) {
+		if (arg.startsWith("-")) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void parseArguments(String[] args) {
+		for (String i: args) {
+			if (isFlag(i)) {
+				if(!argMap.containsKey(i)) {
+					
+				}
+			}
+		}
+	}
+	
     public static void main(String[] args) {
         // TODO Modify as necessary!
-        System.out.println(Arrays.toString(args));
-        
-        String [] argsArray = new String[4];
-        
-        for (int i = 0; i<4; i++) {
-        	argsArray[i] = args[i];
+//        System.out.println(Arrays.toString(args))
+
+    	Map <String,String> argumentMap = new HashMap<>();
+    	
+    	for (int i = 0; i<4; i++) {
+        	if (isFlag(args[i])) {
+        		
+        	}
+        	
         }
-        
-        for (int j = 0; j < 4; j++) {
-        	if (argsArray[j].equals("-dir") | argsArray[j].equals("-index")) {
+
         		// TODO if dir then the next one is the directory to be traversed
         		// no dir = error
         		// if index the it's the json file name.
         		// no filename then default index-simple.json
         		// no -index, still execute but no output file
-        	}
-        }
+        		
+//        	}
+//        }
         
         IndexTest test = new IndexTest();
     }
