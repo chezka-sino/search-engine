e78ed51b1f3ececdc32fae42005ac62994395b9b
chezka-sino <msino@dons.usfca.edu>
2016-09-18 08:20:24 -0700

    WordDataStructure works

--- src/WordDataStructure.java
+++ src/WordDataStructure.java
@@ -3,16 +3,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 public class WordDataStructure {
 	
-	private static Map <String, HashMap <String, ArrayList <Integer>>> fileMap;
+	private static Map <String, HashMap <String, HashSet<Integer>>> fileMap;
 	private static Path inputFile;
 	private static String txtFile;
 	
@@ -56,55 +55,49 @@ public class WordDataStructure {
 	}
 	
 	public void wordMap (String[] words) {
-//		System.out.println(words);
 		
-//		for (String i:words) {
-//			System.out.println(i);
-//		}
-		
-		System.out.println("Word Array SIZE: " + words.length);
-		System.out.println("Initial Map EMPTY: " + fileMap.isEmpty());	
-//		for (int i = 0; i>words.length;i++) {
-//			System.out.println(words[i]);
-//		}
-		
+//		System.out.println("Word Array SIZE: " + words.length);
+//		System.out.println("Initial Map EMPTY: " + fileMap.isEmpty());	
+//		
 		int i = 0;
 		
 		for (String w: words) {
 			System.out.println(w);
 			if (!fileMap.containsKey(w)) {
 				System.out.println("doesn't contain " + w);
-				fileMap.put(w, new HashMap<String, ArrayList<Integer>>());
+				fileMap.put(w, new HashMap<String, HashSet<Integer>>());
 			}
 			
 			if (!fileMap.get(w).containsKey(txtFile)) {
-				fileMap.get(w).put(txtFile, new ArrayList<Integer>());
+				fileMap.get(w).put(txtFile, new HashSet<Integer>());
 			}
 			fileMap.get(w).get(txtFile).add(i);
 			i++;
 		}
 		
-		System.out.println("New Map EMPTY: " + fileMap.isEmpty());
-		System.out.println();
+//		System.out.println("New Map EMPTY: " + fileMap.isEmpty());
+//		System.out.println();
 
 	}
 	
 	public void printMap() {
 		System.out.println("Current Map"); 
-//		System.out.println(fileMap.isEmpty());
-//				
+			
+		for (Map.Entry<String, HashMap<String, HashSet<Integer>>> wordEntry: fileMap.entrySet()) {
+			String word = wordEntry.getKey();
+			System.out.println("WORD: " + word);
+			
+			for (Map.Entry<String, HashSet<Integer>> fileEntry: wordEntry.getValue().entrySet()) {
+				String file = fileEntry.getKey();
+				System.out.println(".TXT FILE: " + file);
+				System.out.println("POSITIONS: " );
+				for (Integer i: fileEntry.getValue()) {
+					System.out.println(i);
+				}
+			}
+			System.out.println();
+		}
 		
-		
-//		for (Map.Entry<String, HashMap<String, ArrayList<Integer>>> entry: fileMap.entrySet()) {
-//			System.out.println("WORD: " + entry.getKey());
-//			for (Map.Entry<String, ArrayList<Integer>> entry2: fileMap.get(entry).entrySet()) {
-//				System.out.println(".TXT FILE: " + entry2.getKey());
-//				System.out.println("POSITIONS: ");
-//				for (Integer c: fileMap.get(entry).get(entry2)) {
-//					System.out.println(c);
-//				}
-//			}
-//		}
 	}
 
 }
